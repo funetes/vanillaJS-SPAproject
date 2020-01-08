d02d12e4b373bd481375f44e87d8e89cf73b1887
Tae Hee Kim <rotoshine@gmail.com>
2020-01-08 15:11:43 +0900

    랜덤하게 에러 발생하게 하고 리스트 API에선 정해진 필드만 내려주도록 함

--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { delay } = require("lodash");
 const path = require("path");
 const fs = require("fs");
 const router = express.Router();
@@ -10,6 +11,12 @@ const resolvingData = readFile(dataFilePath);
 
 const allDataCount = JSON.parse(fs.readFileSync("data.json").toString()).length;
 
+const ERROR_ENCOUNT_PERCENT = 10;
+const MAX_DELAY_TIME = 3000;
+
+const getError = () => getRandomNumber(0, 100) <= ERROR_ENCOUNT_PERCENT;
+
+/* 필요 시 부활시킬 것
 router.get("/cats/all", (req, res) => {
   const page = Number(req.query.page);
 
@@ -41,6 +48,13 @@ router.get("/cats/all", (req, res) => {
       });
     });
 });
+*/
+
+const convertToForList = ({ id, url, name }) => ({
+  id,
+  url,
+  name
+});
 
 router.get("/cats/random50", (req, res) => {
   const minIndex = 0;
@@ -49,19 +63,28 @@ router.get("/cats/random50", (req, res) => {
     getRandomNumber(minIndex, maxIndex)
   );
 
-  resolvingData
-    .then(data => {
-      return res.status(200).send({
-        data: data.filter((_, index) => randomFiftyIndice.includes(index))
-      });
-    })
-    .catch(error => {
-      console.error(error);
-
+  delay(() => {
+    if (getError()) {
       return res.status(500).send({
-        message: "An error has occurred while fetching data."
+        message: "This is an intentional error."
       });
-    });
+    }
+    resolvingData
+      .then(data => {
+        return res.status(200).send({
+          data: data
+            .filter((_, index) => randomFiftyIndice.includes(index))
+            .map(convertToForList)
+        });
+      })
+      .catch(error => {
+        console.error(error);
+
+        return res.status(500).send({
+          message: "An error has occurred while fetching data."
+        });
+      });
+  }, getRandomNumber(0, MAX_DELAY_TIME));
 });
 
 router.get("/cats/search", (req, res) => {
@@ -77,26 +100,34 @@ router.get("/cats/search", (req, res) => {
   const startIndex = (page - 1) * pagingUnit;
   const endIndex = startIndex + pagingUnit;
 
-  resolvingData
-    .then(data => {
-      return res.status(200).send({
-        data: data
-          .filter(
-            ({ name, temperament, origin }) =>
-              name.toLowerCase().includes(q.toLowerCase()) ||
-              temperament.toLowerCase().includes(q.toLowerCase()) ||
-              origin.toLowerCase().includes(q.toLowerCase())
-          )
-          .slice(startIndex, endIndex)
-      });
-    })
-    .catch(error => {
-      console.error(error);
-
+  delay(() => {
+    if (getError()) {
       return res.status(500).send({
-        message: "An error has occurred while fetching data."
+        message: "This is an intentional error."
       });
-    });
+    }
+    resolvingData
+      .then(data => {
+        return res.status(200).send({
+          data: data
+            .filter(
+              ({ name, temperament, origin }) =>
+                name.toLowerCase().includes(q.toLowerCase()) ||
+                temperament.toLowerCase().includes(q.toLowerCase()) ||
+                origin.toLowerCase().includes(q.toLowerCase())
+            )
+            .slice(startIndex, endIndex)
+            .map(convertToForList)
+        });
+      })
+      .catch(error => {
+        console.error(error);
+
+        return res.status(500).send({
+          message: "An error has occurred while fetching data."
+        });
+      });
+  }, getRandomNumber(0, MAX_DELAY_TIME));
 });
 
 router.get("/cats/:id", (req, res) => {
@@ -107,28 +138,34 @@ router.get("/cats/:id", (req, res) => {
       message: "Parameter(id) should be given."
     });
   }
-
-  resolvingData
-    .then(data => {
-      const foundCat = data.find(cat => cat.id === id);
-
-      if (!foundCat) {
-        return res.status(400).send({
-          message: "The gif image with the given id could not be found."
-        });
-      }
-
-      return res.status(200).send({
-        data: foundCat
-      });
-    })
-    .catch(error => {
-      console.error(error);
-
+  delay(() => {
+    if (getError()) {
       return res.status(500).send({
-        message: "An error has occurred while fetching data."
+        message: "This is an intentional error."
       });
-    });
+    }
+    resolvingData
+      .then(data => {
+        const foundCat = data.find(cat => cat.id === id);
+
+        if (!foundCat) {
+          return res.status(400).send({
+            message: "The gif image with the given id could not be found."
+          });
+        }
+
+        return res.status(200).send({
+          data: foundCat
+        });
+      })
+      .catch(error => {
+        console.error(error);
+
+        return res.status(500).send({
+          message: "An error has occurred while fetching data."
+        });
+      });
+  }, getRandomNumber(0, MAX_DELAY_TIME));
 });
 
 module.exports = router;
