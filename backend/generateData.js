a343372b4d8122b20b94295379b7cb729c7a0890
Tae Hee Kim <rotoshine@gmail.com>
2020-01-15 19:45:08 +0900

    API 크롤링 로직 변경 및 크롤링 해오는 데이터 양을 더 늘림

--- backend/generateData.js
+++ backend/generateData.js
@@ -1,76 +1,12 @@
+const Promise = require("bluebird");
 const fs = require("fs");
 const axios = require("axios");
 const { shuffle } = require("lodash");
 // Use your own API key
 const END_POINT = "https://api.thecatapi.com/v1";
 const apiKey = "e83d2c75-da93-4c9e-81d3-5ff1b27f8dd7";
-
-const nameMap = {
-  Abyssinian: "아비시니안",
-  "American Bobtail": "아메리칸 밥테일",
-  Aegean: "에게안",
-  "Arabian Mau": "아라비안 마우",
-  "Australian Mist": "오스트레일리안 미스트",
-  Balinese: "발리니즈",
-  Bambino: "밤비노",
-  Bengal: "벵갈",
-  Birman: "버먼",
-  Bombay: "봄베이",
-  "British Longhair": "브리티쉬 롱헤어",
-  "British Shorthair": "브리티쉬 숏헤어",
-  Burmese: "버마 고양이",
-  Burmilla: "버밀라",
-  "California Spangled": "캘리포니아 스팽글드",
-  "Chantilly-Tiffany": "샹틀리-티파니",
-  Chartreux: "샤르트뢰",
-  Chausie: "쵸시",
-  Cheetoh: "치토",
-  "Colorpoint Shorthair": "컬러포인트 숏헤어",
-  "Cornish Rex": "코니시 렉스",
-  Cymric: "킴릭",
-  Cyprus: "키프로스 고양이",
-  "Devon Rex": "데본렉스",
-  Donskoy: "돈스코이",
-  "Dragon Li": "드레곤리",
-  "Egyptian Mau": "이집션 마우",
-  "European Burmese": "유러피안 버마 고양이",
-  "Exotic Shorthair": "엑조틱 숏헤어",
-  "Havana Brown": "하바나 브라운",
-  Himalayan: "히말라얀",
-  "Japanese Bobtail": "재패니즈 밥테일",
-  Javanese: "자바니즈",
-  "Khao Manee": "카오 마니",
-  Korat: "코랏",
-  Kurilian: "쿠라리안 밥테일",
-  LaPerm: "라팜",
-  "Maine Coon": "메인쿤",
-  Malayan: "말레이안",
-  Manx: "맹크스",
-  Munchkin: "먼치킨",
-  Nebelung: "네벨룽",
-  "Norwegian Forest Cat": "노르웨이 숲 고양이",
-  Ocicat: "오시캣",
-  Oriental: "오리엔탈 숏헤어",
-  Persian: "페르시안",
-  "Pixie-bob": "픽시 밥",
-  Ragamuffin: "라가머핀",
-  Ragdoll: "랙돌",
-  "Russian Blue": "러시안 블루",
-  Savannah: "사바나",
-  "Scottish Fold": "스코티시 폴드",
-  "Selkirk Rex": "셀커크 렉스",
-  Siamese: "샴 고양이",
-  Siberian: "시베리안",
-  Singapura: "싱가푸라",
-  Snowshoe: "스노우슈",
-  Somali: "소말리 고양이",
-  Sphynx: "스핑크스",
-  Tonkinese: "톤키니즈",
-  Toyger: "토이거",
-  "Turkish Angora": "터키시 앙고라",
-  "Turkish Van": "터키시 반",
-  "York Chocolate": "요크 초콜릿"
-};
+const catNames = require("./catNames");
+const dataValidator = require("./dataValidation");
 
 const limit = 100;
 
@@ -100,29 +36,33 @@ const allRequests = async () => {
   const tasks = [];
 
   for (const breed of breeds) {
-    for (let page = 1; page <= 5; page++) {
+    for (let page = 0; page <= 9; page++) {
+      const apiUrl = `${END_POINT}/images/search?breed_id${breed.id}&limit=${limit}&page=${page}`;
       tasks.push(
         fetcher({
-          url: `${END_POINT}/images/search`,
-          params: {
-            limit,
-            page,
-            breed_id: breed.id
-          }
-        }).then(res => {
-          const { name, temperament, origin } = breed;
-          const { data: cats } = res;
-
-          return cats.map(({ id, url, width, height }) => ({
-            name: `${name} / ${nameMap[name]}`,
-            id,
-            url,
-            width,
-            height,
-            temperament,
-            origin
-          }));
+          url: apiUrl
         })
+          .then(res => {
+            const { name, temperament, origin } = breed;
+            const { data: cats } = res;
+
+            console.log(
+              `${apiUrl} ==> ${breed.name} / page ${page} result: ${cats.length}`
+            );
+            return cats.map(({ id, url, width, height }) => ({
+              name: `${name} / ${catNames[name]}`,
+              id,
+              url,
+              width,
+              height,
+              temperament,
+              origin
+            }));
+          })
+          .catch(e => {
+            console.log(`ERROR: ${apiUrl} ==> ${e.message}`);
+            return [];
+          })
       );
     }
   }
@@ -134,14 +74,10 @@ const allRequests = async () => {
         []
       );
 
-      fs.writeFile(
-        "data.json",
-        JSON.stringify(shuffle(result)),
-        "utf8",
-        () => {}
-      );
-
-      console.log(`crwaling success. total data: ${result.length}`);
+      fs.writeFile("data.json", JSON.stringify(shuffle(result)), "utf8", () => {
+        console.log(`crwaling success. total data: ${result.length}`);
+        dataValidator();
+      });
     })
     .catch(error => {
       console.log("**************************************");
