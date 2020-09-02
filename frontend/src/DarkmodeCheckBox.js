856e70daf843fdf0b0075871669c9e8eb93fa6ac
kimhwan <khan@MacBook-Pro.local>
2020-09-02 10:23:42 +0900

    add 로컬스토리지추가 && 리팩토링

--- frontend/src/DarkmodeCheckBox.js
+++ frontend/src/DarkmodeCheckBox.js
@@ -1,30 +1,50 @@
+const DARK = 'dark';
+const LIGHT = 'light';
+const COLOR_MODE = 'color-mode';
 class DarkmodeCheckBox {
   constructor({ $target }) {
     const $darkModeInput = document.createElement('input');
-    this.$darkModeInput = $darkModeInput;
-    this.$darkModeInput.type = 'checkbox';
-    this.$darkModeInput.classList.add('DarkmodeCheckBox');
-    $target.appendChild($darkModeInput);
-    this.darkmodeInit();
-  }
-  setHtmlAttribute(attribute, isDark) {
-    document.documentElement //
-      .setAttribute(attribute, isDark ? 'dark' : 'light');
-    return;
-  }
-  handleDarkmode(e) {
-    return this.setHtmlAttribute('color-mode', e.target.checked);
-  }
-  darkmodeInit() {
+
     this.prefersDark =
       window.matchMedia &&
       window.matchMedia('(prefers-color-scheme: Dark)').matches;
+    this.$darkModeInput = $darkModeInput;
+    this.$darkModeInput.type = 'checkbox';
 
-    this.setHtmlAttribute('color-mode', this.prefersDark);
-    this.$darkModeInput.checked = this.prefersDark;
-    this.$darkModeInput.addEventListener(
-      'change',
-      this.handleDarkmode.bind(this)
+    $target.appendChild($darkModeInput);
+
+    this.darkmodeInit();
+  }
+  setHtmlAttribute(attribute) {
+    return document.documentElement.setAttribute(
+      attribute,
+      localStorage.getItem(COLOR_MODE)
     );
+  }
+  handleChange(e) {
+    const { setHtmlAttribute } = this;
+    const {
+      target: { checked },
+    } = e;
+    localStorage.setItem(COLOR_MODE, checked ? DARK : LIGHT);
+    return setHtmlAttribute(COLOR_MODE);
+  }
+  darkmodeInit() {
+    const {
+      $darkModeInput,
+      setHtmlAttribute,
+      handleChange,
+      prefersDark,
+    } = this;
+
+    localStorage.getItem(COLOR_MODE) ||
+      localStorage.setItem(COLOR_MODE, prefersDark ? DARK : LIGHT);
+
+    setHtmlAttribute(COLOR_MODE);
+
+    $darkModeInput.checked = localStorage.getItem(COLOR_MODE) === DARK;
+
+    $darkModeInput.addEventListener('change', handleChange.bind(this));
+    return;
   }
 }
